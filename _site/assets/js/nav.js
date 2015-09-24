93975e7b4ca2c073cc7a27a4853a2d9700ed16a9
asipple1 <asipple1@gmail.com>
2015-09-23 19:39:52 -0500

    more styling to work section

--- _site/assets/js/nav.js
+++ _site/assets/js/nav.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.hamburger').click(function () {
-    $('#sidebar-wrapper').css("margin-left", "-250px");
+    $('#sidebar-wrapper').css("margin-left", "-250px").css("margin-top", "-60px");
   });
   $('#exit').click(function () {
     $('#sidebar-wrapper').css("margin-left", "-1000px");
